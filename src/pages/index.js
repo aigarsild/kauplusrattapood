1b5279e46b78416468fe4cbf7f074e47a7a16c66
Aigar Sild <aigar.sild@vaimo.com>
2020-05-14 12:25:04 +0300

    Text fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ class Home extends Component {
       <input type="text" name="origin" />
       <textarea name="message"></textarea>
     </form>
-    <SEO title="Jalgratta hooldus ja parandus" />
+    <SEO title="Ratta hooldus ja parandus" description="Kauplus rattapood parandab ja hooldab rattaid, ratta hoolduse hinnakiri" />
                 <section className="row slider d-flex align-content-center flex-wrap min-vh-100">
                     <h1 className="display-4 text-center w-100 font-weight-bold mb-4">Mobiilne ratta hooldus ja parandus</h1>
                     <div className="lead text-center w-100">
@@ -30,26 +30,7 @@ class Home extends Component {
                     </div>
 
                     <div className="w-100 text-center">
-                        <a href="#hinnakiri" className="shadow btn btn-dark font-weight-bold m-4 pt-3 pb-3 pl-5 pr-5">Vaata hinnakirja</a>
-                    </div>
-                </section>
-
-                <section className="row slider d-flex align-content-center flex-wrap min-vh-100">
-                    <div className="container">
-                        <div className="row">
-                            <h2 className="display-4 text-center w-100 font-weight-bold mb-4">On jalgrattahooaeg! Kõik ratturid on juba õues</h2>
-                            <div className="lead text-center w-100">
-                                Sõidetakse metsas, poes ja niisama aleviku vahel.
-                                Lapsed tiirutavad peale koolipäeva, et saaks koolimõtted peast ning kohtuda mõne sõbraga, et koos sõita ja seiklusi otsida.
-                                Noored kasutavad ratast kui tavalist sõiduvahendit, millega oma peika või pruta juurde reisida.
-                                Vahel on kondimootoriga ka hea raamatukokku minna.
-                                Ja tööinimesed naudivad raudhobuga sõitu peale väljakutsuvat tööpäeva. Lõõgastav matk looduses, mmm!
-                            <p>
-                                    Jalgratas on igapäevane asi. Nii nagu hooldad ennast ja kõiki asju, mida kasutad iga päev, tuleb hooldada ka ratast.
-                            </p>
-                                <p>Laagri rattapood hooldab meeleldi sinu ratast.</p>
-                            </div>
-                        </div>
+                        <a href="#hinnakiri" className="shadow btn btn-dark font-weight-bold m-4 pt-3 pb-3 pl-5 pr-5">Vaata rattahooldus hinnakirja</a>
                     </div>
                 </section>
 
